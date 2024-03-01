f55b3e44fd7ca8e895e2972cab4e0bb6f8d2761b
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-01 23:30:59 +0530

    Update form.php

--- form.php
+++ form.php
@@ -17,14 +17,39 @@ $_country=$_GET["_country"];
 $_zip=$_GET["_zip"];
 $date=$_GET["date"];
 $subject=$_GET["subject"];
-$desc=$_GET["desc"];
+$description=$_GET["description"];
 $consent=$_GET["consent"];
 
 $conn=mysqli_connect("localhost","root","","NGO_database");
-$sql="INSERT INTO reports VALUES('$fname','$lname','$address','$city','$state','$country','$zip','$tel','$mail','$_address','$_city','$_state','$_country','$_zip''$date','$subject','$desc','$consent')";
-if($conn->query(sql)===TRUE)
+$sql="INSERT INTO user VALUES('$fname','$lname','$address','$city','$state','$country','$zip','$tel','$mail')";
+if($conn->query($sql)===TRUE)
 {
-echo "Submitted";
+$sql="INSERT INTO report VALUES('$_address','$_city','$_state','$_country','$_zip','$date','$subject','$description')";
+if($conn->query($sql)===TRUE)
+{
+echo "
+<h1>Thank You!!!!!</h1><br>
+<p>
+First name: $fname<br>
+Last name: $lname<br>
+Address: $address<br>
+City: $city<br>
+State: $state<br>
+Country: $country<br>
+Pin code: $zip<br>
+Telephone: $tel<br>
+E-mail: $mail<br>
+Address where animal is located: $address<br>
+City where animal is located: $_city<br>
+State where animal is located: $_state<br>
+Country where animal is located: $country<br>
+Zip code where animal is located: $zip<br>
+Date observed: $date<br>
+Subject: $subject<br>
+Please describe your concern: $description
+</p>
+";
+}
 }
 ?>
 </body>

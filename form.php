4b53f5d6bea65f22b6115b94fa791090460b7918
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-01 17:36:46 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,1 +1,29 @@
+<html>
+<body>
+<?php
+$fname=$_GET["fname"];
+$lname=$_GET["lname"];
+$address=$_GET["address"];
+$city=$_GET["city"];
+$state=$_GET["state"];
+$country=$_GET["country"];
+$zip=$_GET["zip"];
+$tel=$_GET["tel"];
+$mail=$_GET["mail"];
+$_address=$_GET["_address"];
+$_city=$_GET["_city"];
+$_state=$_GET["_state"];
+$_country=$_GET["_country"];
+$_zip=$_GET["_zip"];
+$_tel=$_GET["_tel"];
+$_mail=$_GET["_mail"];
 
+$conn=mysqli_connect("localhost","root","","NGO_database");
+$sql="INSERT INTO reports VALUES('$fname',)";
+if($conn->query(sql)===TRUE)
+{
+echo "Submitted";
+}
+?>
+</body>
+</html>

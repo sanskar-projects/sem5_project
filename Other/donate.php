a9d60d8dc20120c9e2d98c4be37545415554463f
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-03 00:00:12 +0530

    Create donate.php

---
+++ Other/donate.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+$fullname=$_GET["fullname"];
+$email=$_GET["email"];
+$address=$_GET["address"];
+$city=$_GET["city"];
+$state=$_GET["state"];
+$fund=$_GET["fund"];
+
+$conn=mysqli_connect("localhost","root","","team1database");
+$sql="INSERT INTO donate VALUES('$fullname','$email','$address','$city','$state','$fund')";
+if($conn->query($sql)===TRUE)
+{
+echo "<h1>Thank you for donating!</h1>";
+?>
+</body>
+</html>

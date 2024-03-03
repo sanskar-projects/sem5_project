3160ecca5a285cfa13207f4a59c4daf794c6a627
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-03 20:54:48 +0530

    Update query.php

--- Other/query.php
+++ Other/query.php
@@ -3,14 +3,12 @@
 <?php
 $fullname=$_GET["fullname"];
 $email=$_GET["email"];
-$address=$_GET["address"];
-$city=$_GET["city"];
-$state=$_GET["state"];
-$fund=$_GET["fund"];
+$phone=$_GET["phone"];
+$query=$_GET["query"];
 if(!empty($email))
 {
 $conn=mysqli_connect("localhost","root","","team1database");
-$sql="INSERT INTO donate VALUES('$fullname','$email','$address','$city','$state','$fund')";
+$sql="INSERT INTO donate VALUES('$fullname','$email','$phone','$query')";
 if($conn->query($sql)===TRUE)
 {
 echo "<h1>Thank you for donating!</h1>";

b4f316abe54c65f5ed5615e8f7f762223f20b081
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-03 17:38:08 +0530

    Update Query.js

--- Other/Query.js
+++ Other/Query.js
@@ -1,11 +1,9 @@
-function donatevalidate()
+function validate()
 {
 var fullname=document.getElementById("fullname");
 var email=document.getElementById("email");
-var address=document.getEdonatelementById("address");
-var city=document.getElementById("city");
-var state=document.getElementById("state");
-var fund=document.getElementById("fund");
+var phone=document.getEdonatelementById("phone");
+var query=document.getElementById("query");
 var EMAIL=/^[a-zA-Z0-9._]+@[a-zA-Z.]+$/;
 if(fullname.value.length==0)
 {
@@ -19,29 +17,17 @@ alert("enter email");
 document.getElementById("email")="";
 return;
 }
-if(address.value.length==0)
+if(phone.value.length==0)
 {
 alert("enter address");
 document.getElementById("email")="";
 return;
 }
-if(city.value.length==0)
+if(query.value.length==0)
 {
 alert("enter city");
 document.getElementById("email")="";
 return;
-}
-if(state.value.length==0)
-{
-alert("enter state");
-document.getElementById("email")="";
-return;
-}
-if(fund.selectedIndex==false)
-{
-alert("select fund");
-document.getElementById("email")="";
-return
 }
 if(!email.value.match(EMAIL))
 {

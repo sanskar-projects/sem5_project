acdb7d5442ff06fb4347c68a34c338ac833fdf5f
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-03 17:52:56 +0530

    Update Report.js

--- Other/Report.js
+++ Other/Report.js
@@ -18,79 +18,79 @@ var EMAIL=/^[a-zA-Z0-9._]+@[a-zA-Z.]+$/;
 
 if(fname.value.length==0)
 {
-alert("enter fullname");
+alert("enter firstname");
 document.getElementById("email")="";
 return;
 }
 if(lname.value.length==0)
 {
-alert("enter email");
+alert("enter lastname");
 document.getElementById("email")="";
 return;
 }
 if(city.value.length==0)
 {
-alert("enter address");
+alert("enter city");
 document.getElementById("email")="";
 return;
 }
 if(state.value.length==0)
 {
-alert("enter city");
+alert("enter state");
 document.getElementById("email")="";
 return;
 }
 if(email.value.length==0)
 {
-alert("enter state");
+alert("enter email");
 document.getElementById("email")="";
 return;
 }
 if(phone.value.length==0)
 {
-alert("enter state");
+alert("enter phone");
 document.getElementById("email")="";
 return;
 }
 if(addressofanimal.value.length==0)
 {
-alert("enter state");
+alert("enter address of animal");
 document.getElementById("email")="";
 return;
 }
 if(cityofanimal.value.length==0)
 {
-alert("enter state");
+alert("enter city of animal");
 document.getElementById("email")="";
 return;
 }
 if(stateofanimal.value.length==0)
 {
-alert("enter state");
+alert("enter state of animal");
 document.getElementById("email")="";
 return;
 }
 if(countryofanimal.value.length==0)
 {
-alert("enter state");
+alert("enter country of animal");
 document.getElementById("email")="";
 return;
 }
 if(date.value.length==0)
 {
-alert("enter state");
+alert("enter date");
 document.getElementById("email")="";
 return;
 }
 if(pincode.value.length==0)
 {
-alert("enter state");
+alert("enter pincode");
 document.getElementById("email")="";
 return;
 }
 if(concern.value.length==0)
 {
-alert("enter state");
+alert("enter concern");
 document.getElementById("email")="";
 return;
 }

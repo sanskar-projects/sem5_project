6d53a2f18a18f984f0f857e474a5a6e04354fa0b
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-03 15:31:27 +0530

    Create report.php

---
+++ Other/report.php
@@ -0,0 +1,19 @@
+<form method="get" action="report.php">
+
+<input name="fname">
+<input name="lname">
+<input name="city">
+<input name="state">
+<input name="email">
+<input name="phone">
+<input name="addressofanimal">
+<input name="cityofanimal">
+<input name="stateofanimal">
+<input name="countryofanimal">
+<input name="date">
+<input name="pincode">
+<input name="concern">
+
+<input type="submit">
+
+</form>

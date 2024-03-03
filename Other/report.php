f525a159251f431990ab19261d3765dcba32459d
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-03 21:42:15 +0530

    Update report.php

--- Other/report.php
+++ Other/report.php
@@ -1,20 +1,25 @@
 <html>
 <body>
 <?php
-$fullname=$_GET["fullname"];
-$email=$_GET["email"];
-$address=$_GET["address"];
+$fname=$_GET["fname"];
+$lname=$_GET["lname"];
 $city=$_GET["city"];
 $state=$_GET["state"];
-$fund=$_GET["fund"];
-if(!empty($email))
-{
+$email=$_GET["email"];
+$phone=$_GET["phone"];
+$addressofanimal=$_GET["addressofanimal"];
+$cityofanimal=$_GET["cityofanimal"];
+$stateofanimal=$_GET["stateofanimal"];
+$countryofanimal=$_GET["countryofanimal"];
+$date=$_GET["date"];
+$pincode=$_GET["pincode"];
+$concern=$_GET["concern"];
+
 $conn=mysqli_connect("localhost","root","","team1database");
-$sql="INSERT INTO donate VALUES('$fullname','$email','$address','$city','$state','$fund')";
+$sql="INSERT INTO report VALUES('$fname','$lname','$city','$state','$email','$phone','$addressofanimal','$cityofanimal','$stateofanimal','$countryofanimal','$date','$pincode','$concern')";
 if($conn->query($sql)===TRUE)
 {
 echo "<h1>Thank you for donating!</h1>";
-}
 }
 else
 {

463e09cb1d1dd589c693eedd058755b60d512173
Sanskar <117817010+sanskar-projects@users.noreply.github.com>
2024-03-04 07:47:20 +0530

    Update reportNEW.php

--- Other/reportNEW.php
+++ Other/reportNEW.php
@@ -1,22 +1,16 @@
 <html>
 <body>
 <?php
-$fname=$_GET["fname"];
-$lname=$_GET["lname"];
-$city=$_GET["city"];
-$state=$_GET["state"];
+$animal=$_GET["animal"];
+$adopter=$_GET["adopter"];
 $email=$_GET["email"];
 $phone=$_GET["phone"];
-$addressofanimal=$_GET["addressofanimal"];
-$cityofanimal=$_GET["cityofanimal"];
-$stateofanimal=$_GET["stateofanimal"];
-$countryofanimal=$_GET["countryofanimal"];
 $date=$_GET["date"];
-$pincode=$_GET["pincode"];
-$concern=$_GET["concern"];
 
+if(isset($_GET["agree"]))
+{
 $conn=mysqli_connect("localhost","root","","team1database");
-$sql="INSERT INTO report VALUES('$fname','$lname','$city','$state','$email','$phone','$addressofanimal','$cityofanimal','$stateofanimal','$countryofanimal','$date','$pincode','$concern')";
+$sql="INSERT INTO adopt VALUES('$animal','$adopter','$email','$phone','$date')";
 if($conn->query($sql)===TRUE)
 {
 echo "<h1>Thank you for donating!</h1>";
@@ -24,6 +18,11 @@ echo "<h1>Thank you for donating!</h1>";
 else
 {
 echo "enter valid details";
+}
+}
+else
+{
+echo "please tick the consent box";
 }
 ?>
 </body>
